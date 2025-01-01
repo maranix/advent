f8f30770ab07b47c4c1d7d3ebfee9a5d57521d1f
maranix <ramanverma4183@gmail.com>
2025-01-01 11:50:37 +0530

    zig is a no-go.
    * Don't want to implement everything from scratch

--- adventy/src/http/http.zig
+++
@@ -1,3 +0,0 @@
-pub const Server = @import("Server.zig");
-
-pub const HttpError = error{};
--- adventy/src/main.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-const http = @import("http/http.zig");
-
-pub fn main() !void {
-    var server = try http.Server.init(.{});
-    try server.start();
-}
