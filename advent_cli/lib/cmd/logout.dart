9927b557888a2ddfa8b84fa7f5e40414c446339b
maranix <ramanverma4183@gmail.com>
2025-01-06 11:32:04 +0530

    feat(cmd/logout): locate and delete session token

--- advent_cli/lib/cmd/logout.dart
+++ advent_cli/lib/cmd/logout.dart
@@ -1,6 +1,9 @@
 import "dart:io";
 
 import "package:args/command_runner.dart";
+import "package:advent_cli/constants.dart";
+import "package:advent_cli/util/apputil.dart" as apputil;
+import "package:path/path.dart" as path;
 
 final class LogoutCommand extends Command {
   @override
@@ -11,9 +14,35 @@ final class LogoutCommand extends Command {
 
   @override
   void run() async {
-    final file = File(".session");
-    await file.delete();
+    final cwdPath = "./$SESSION_TOKEN_FILE_NAME";
+    final configPath = path.join(
+      apputil.applicationConfigDir().path,
+      SESSION_TOKEN_FILE_NAME,
+    );
 
-    print("session token deleted");
+    final sessionExists = await Future.wait(
+      [
+        FileSystemEntity.isFile(cwdPath),
+        FileSystemEntity.isFile(configPath),
+      ],
+      eagerError: true,
+    );
+
+    try {
+      for (final (i, exists) in sessionExists.indexed) {
+        if (!exists) continue;
+
+        if (i == 0) {
+          await File(cwdPath).delete();
+        } else {
+          await File(configPath).delete();
+        }
+      }
+
+      print("session token deleted");
+    } on Exception catch (e) {
+      print(e.toString());
+      return;
+    }
   }
 }
